f8072b36315605c76d4f7776d8529c87c75ec46f
bekaneo <vealtto@gmail.com>
2022-08-09 17:54:34 +0600

    rebuilded in funcs

--- main.py
+++ main.py
@@ -1,42 +1,78 @@
-from datetime import datetime
 import os
+from datetime import datetime
+from typing import List, Generator
 
 import requests
 from bs4 import BeautifulSoup
+from bs4.element import ResultSet
 import csv
 
-url = r"https://www.akchabar.kg/ru/exchange-rates/"
+URL = "https://www.akchabar.kg/ru/exchange-rates/"
 
-response = requests.get(url)
-html_page = response.text
 
-soup = BeautifulSoup(html_page, 'html.parser')
-tables = soup.find_all("table", {'id': 'rates_table'})
-for index, table in enumerate(tables):
+def get_response(url: str) -> requests:
+    return requests.get(url)
 
-    table_rows = table.find_all("tr")
 
-    with open(f"{datetime.now().date()}_valuta_kg.csv", "a+", newline="") as file:
-        writer = csv.writer(file)
-        file.seek(0, os.SEEK_END)
-        if file.tell():
-            writer.writerow([str(datetime.now().time())[:5]] + ['–––––––' for x in table_rows])
+def get_html(response: requests) -> requests:
+    return response.text
+
+
+def get_table(html: requests) -> ResultSet:
+    soup = BeautifulSoup(html, 'html.parser')
+    return soup.find_all("table", {'id': 'rates_table'})
+
+
+def get_headers(table: ResultSet) -> List[str]:
+    headers = []
+    for item in table:
+        table_header = item.find_all('th')
+        if table_header:
+            for th in table_header:
+                th = th.text.strip()
+                if th in ['USD', 'EURO', 'RUB', 'KGZ']:
+                    headers.append(th)
+                    headers.append('')
+                else:
+                    headers.append(th)
+    return headers
+
+
+def get_tr(tables: ResultSet) -> Generator:
+    for table in tables:
+        table_rows = table.find_all("tr")
         for row in table_rows:
-            row_data = []
-            if row.find_all("th"):
-                table_headings = row.find_all("th")
-                header_th = []
-                for item in table_headings:
-                    item = item.text.strip()
-                    if item in ['USD', 'EURO', 'RUB', 'KGZ']:
-                        header_th.append(item)
-                        header_th.append('')
-                    else:
-                        header_th.append(item)
-                for th in header_th:
-                    row_data.append(th)
-            else:
-                table_data = row.find_all("td")
-                for td in table_data:
-                    row_data.append(td.text.strip())
-            writer.writerow(row_data)
+            if row.find_all("td"):
+                yield row.find_all("td")
+
+
+def get_td(table_data: Generator) -> Generator:
+    for rows in table_data:
+        row_data = []
+        for td in rows:
+            row_data.append(td.text.strip())
+        if row_data:
+            yield row_data
+
+
+def write_csv(headers: List[str], data: Generator) -> None:
+    with open(f"{datetime.now().date()}_valuta.csv", "a+", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([str(datetime.now().time())[:5]] + ['––––––' for x in range(8)])
+        writer.writerow(headers)
+        for row in data:
+            writer.writerow(row)
+
+
+def main(url: str) -> None:
+    response = get_response(url)
+    html = get_html(response)
+    table = get_table(html)
+    headers = get_headers(table)
+    trs = get_tr(table)
+    tds = get_td(trs)
+    write_csv(headers, tds)
+
+
+if __name__ == "__main__":
+    main(URL)
